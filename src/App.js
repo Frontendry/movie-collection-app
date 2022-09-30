f1617f36a1ce88949548dfe1157d279b67b39bcd
frontendry <brngatarwa@gmail.com>
2022-09-30 16:33:34 +0300

    genre set up

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { Routes, Route } from "react-router-dom";
 
 // Components
 import Home from "./components/pages/home/Home";
+import Genre from "./components/pages/genres/Genres";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="genre/:genreName" element={<Genre />} />
     </Routes>
   );
 }
---
+++ src/components/pages/genres/Genres.js
@@ -0,0 +1,40 @@
+// React Router Modules
+import { useParams } from "react-router-dom";
+
+// Context Store
+import { useFilmsContext } from "../../../context/FilmsContextStore";
+
+// Components
+import Movies from "../../general-elements/movies/Movies";
+import Header from "../../general-elements/header/Header";
+import SearchWrapper from "../../general-elements/search/SearchWrapper";
+
+const Genre = () => {
+  // Get Param Name
+  const { genreName } = useParams();
+
+  // Capitalize Param Name
+  const capitalizeGenreName =
+    genreName.charAt(0).toUpperCase() + genreName.slice(1);
+
+  // All Films
+  const { allFilms } = useFilmsContext();
+
+  // Filter Selected Genre
+  const genreNameCollection =
+    allFilms &&
+    allFilms.filter((film) => film.Genre.toLowerCase().includes(genreName));
+
+  return (
+    <main className="min-h-screen bg-slate-900">
+      <Header />
+      <SearchWrapper />
+      <Movies
+        moviesCollection={genreNameCollection}
+        collectionTitle={capitalizeGenreName}
+      />
+    </main>
+  );
+};
+
+export default Genre;
