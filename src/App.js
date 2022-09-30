86d5f0efd073a05076620e73ddbce29c34b349f2
frontendry <brngatarwa@gmail.com>
2022-09-30 07:03:15 +0300

    test paths

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+// React Router Modules
+import { Routes, Route } from "react-router-dom";
+
+// Components
+import Home from "./components/pages/home/Home";
+
 function App() {
-  return <div className="App">teststs</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  );
 }
 
 export default App;
---
+++ src/components/pages/home/Home.js
@@ -0,0 +1,11 @@
+import SearchWrapper from "../../general-elements/search/SearchWrapper";
+
+const Home = () => {
+  return (
+    <div>
+      Home <SearchWrapper />
+    </div>
+  );
+};
+
+export default Home;
